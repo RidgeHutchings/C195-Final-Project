b09df25723e0a031839765a8ca7556e2d0dd93ce
Ridge Hutchings <RidgeHutchings@gmail.com>
2021-03-21 04:56:25 -0600

    added appointment table functionality

--- src/sample/MainScreenController.java
+++ src/sample/MainScreenController.java
@@ -4,12 +4,15 @@ import com.mysql.jdbc.Statement;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import utils.DBQuery;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -17,8 +20,7 @@ import java.util.List;
 public class MainScreenController {
     @FXML
     TableView<Customer> customerTable;
-    @FXML
-    TableView appointmentsTable;
+
     @FXML
     TableColumn<Customer,Integer> customerIDCol;
     @FXML
@@ -39,6 +41,38 @@ public class MainScreenController {
     TableColumn<Customer,Integer> customerDivisionIDCol;
     @FXML
     TableColumn<Customer,String> customerAddressCol;
+    //appointment table FXML below
+    @FXML
+    TableView appointmentTable;
+    @FXML
+    TableColumn<Appointments,Integer> appointmentIDCol;
+    @FXML
+    TableColumn<Appointments,String>appointmentTitleCol;
+    @FXML
+    TableColumn<Appointments,String>appointmentDescriptionCol;
+    @FXML
+    TableColumn<Appointments,String>appointmentLocationCol;
+    @FXML
+    TableColumn<Appointments,String>appointmentTypeCol;
+    @FXML
+    TableColumn<Appointments,String>appointmentStartCol;
+    @FXML
+    TableColumn<Appointments,String>appointmentEndCol;
+    @FXML
+    TableColumn<Appointments,String>appointmentCreateDateCol;
+    @FXML
+    TableColumn<Appointments,String>appointmentCreatedByCol;
+    @FXML
+    TableColumn<Appointments,String>appointmentLastUpdateCol;
+    @FXML
+    TableColumn<Appointments,String>appointmentLastUpdatedByCol;
+    @FXML
+    TableColumn<Appointments,Integer>appointmentCustomerIDCol;
+    @FXML
+    TableColumn<Appointments,Integer>appointmentUserIDCol;
+    @FXML
+    TableColumn<Appointments,Integer>appointmentContactIDCol;
+
 
     @FXML
     private void initialize() throws SQLException {
@@ -75,5 +109,47 @@ public class MainScreenController {
 
 
 
+    }
+
+    //This method populates the appointment table based on the selected customer
+    public void populateAppointmentTable(MouseEvent event) throws IOException, SQLException {
+        Customer selectedCustomer = customerTable.getSelectionModel().getSelectedItem();
+        //clear the table in case there are previous entries
+        appointmentTable.getItems().clear();
+        //Get the Statement reference
+        Statement statement = DBQuery.getStatement();
+
+        //run an updated SQL query on the customer selected
+        ResultSet results = statement.executeQuery("SELECT * FROM appointments WHERE Customer_ID='"+selectedCustomer.getID()+"'");
+
+        //populate the appointment table
+        while(results.next())
+        {
+            System.out.println("populating appointment table now!");
+            Appointments appoint = new Appointments(results.getInt("Appointment_ID"),results.getString("Title"),results.getString("Description"),results.getString("Location"),
+                    results.getString("Type"), results.getString("Start"),results.getString("End"),results.getString("Create_Date"),
+                    results.getString("Created_By"),results.getString("Last_Update"), results.getString("Last_Updated_By"),results.getInt("Customer_ID"),
+                    results.getInt("User_ID"),results.getInt("Contact_ID"));
+            Appointments.getAppointments().add(appoint);
+
+        }
+        //if there are no appointments, the table is not populated
+        if(Appointments.getAppointments()!=null) {
+            appointmentTable.setItems(Appointments.getAppointments());
+            appointmentIDCol.setCellValueFactory(new PropertyValueFactory<>("appointmentID"));
+            appointmentTitleCol.setCellValueFactory(new PropertyValueFactory<>("Title"));
+            appointmentDescriptionCol.setCellValueFactory(new PropertyValueFactory<>("Description"));
+            appointmentLocationCol.setCellValueFactory(new PropertyValueFactory<>("Location"));
+            appointmentTypeCol.setCellValueFactory(new PropertyValueFactory<>("Type"));
+            appointmentStartCol.setCellValueFactory(new PropertyValueFactory<>("Start"));
+            appointmentEndCol.setCellValueFactory(new PropertyValueFactory<>("End"));
+            appointmentCreateDateCol.setCellValueFactory(new PropertyValueFactory<>("CreateDate"));
+            appointmentCreatedByCol.setCellValueFactory(new PropertyValueFactory<>("CreatedBy"));
+            appointmentLastUpdateCol.setCellValueFactory(new PropertyValueFactory<>("LastUpdate"));
+            appointmentLastUpdatedByCol.setCellValueFactory(new PropertyValueFactory<>("LastUpdatedBy"));
+            appointmentCustomerIDCol.setCellValueFactory(new PropertyValueFactory<>("CustomerID"));
+            appointmentUserIDCol.setCellValueFactory(new PropertyValueFactory<>("UserID"));
+            appointmentContactIDCol.setCellValueFactory(new PropertyValueFactory<>("ContactID"));
+        }
     }
 }
